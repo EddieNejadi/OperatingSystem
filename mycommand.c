c6f14ca0cf2aa54bee7b2dc657cd7474e4161a8c
Mehdi A.Nejadi <mehdi61b@gmail.com>
2014-11-27 20:49:38 +0100

    Fixed signal handling for Cntrl + c and some other bugs

--- mycommand.c
+++ mycommand.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
 #include "parse.h"
 #include "mycommand.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <stdbool.h>
 
-#include <sys/wait.h>
 
 extern char **environ;
-static pid_t ch;
+static pid_t parent_pid;
+static pid_t child_pid;
+static pid_t bgp;
 
 
 
 void exitHandler(int sig) 
 {
 	printf("\nin exitHandler signal is: %d and pid is: %d\n", sig, getpid());
-    if (ch != NULL)
+
+    if (parent_pid != getpid() && bgp != getpgid(getpid()) )
     {
-    	kill(ch,9);
+		printf("\n**** getpid() is: %d\n", getpid());
+    	// exit(-1);
+    	kill(getpgid(getpid()), sig);
+
     }
     // return 0;
 }
 
 void runCommand(Command *cmd)
 {
-	ch = NULL;
+	parent_pid = getpid();
 	char **pl = cmd->pgm->pgmlist;
 	int bg = (int) cmd->bakground;
 	// printf ("%i\n", bg);
@@ -43,36 +48,57 @@ void runCommand(Command *cmd)
 	}
 	pid_t res;
 	res = NULL;
+	bgp = NULL;
 	// int pfd[2];	
 	// pipe(pfd);
-	signal(SIGCHLD, SIG_IGN);
 	signal(SIGINT, exitHandler);
-	if (bg == 0)
-	{
-		ch = fork();
-	}else
-		res = fork();
-	{
-	}
-	if ((ch < 0 && res == NULL) || (res < 0 && ch == NULL))
+	signal(SIGCHLD, SIG_IGN);
+	res = fork();
+
+	// if (bg == 0)
+	// {
+	// 	ch = fork();
+	// }else
+	// {
+	// }
+	// if ((ch < 0 && res == NULL) || (res < 0 && ch == NULL))
+	if (res < 0)
 	{
 		printf("fork get error\n");
 	}
-	if ((ch > 0 && res == NULL) || (res > 0 && ch == NULL)) // Parent process
+	// if ((ch > 0 && res == NULL) || (res > 0 && ch == NULL)) // Parent process
+	if (res > 0) // Parent process
 	{
-		if (bg == 0) // Check if the ch is not background process
+		if (bg == 0) // Check if the child is not background process
 		{
-			printf("Parent pid is: %d and ch is %d\n", getpid(), ch);
-			if (ch > 0) waitpid(ch,NULL,NULL);
-			else waitpid(res,NULL,NULL);
+			printf("Parent pid is: %d\n", getpid());
+			child_pid = res;
+			waitpid(res,NULL,NULL);
+			// wait(NULL);
+			// if (ch > 0) waitpid(ch,NULL,NULL);
+			// else waitpid(res,NULL,NULL);
 			// while(c)
 			// {
 
 			// }	
 			printf("DONE\n");
+		}
+		else // Background process
+		{
+			if (bgp > 0 && bgp != NULL)
+			{
+				setpgid(getpid(), bgp);
+			}
+			else
+			{
+				bgp = setsid();
+				setpgid(bgp, 0);
+				printf("bgp= %d bgpgid%d\n",bgp,getpgid(getpid()));
+			}
+
 		} 
 	}
-	else // ch process
+	else // Child process
 	{
 		FILE *output, *input;
 		input = NULL;
@@ -107,7 +133,8 @@ int runCmds(Command *cmd,Pgm *p)
 		return 0;
 	}
 	pid_t pid;
-	
+	// signal(SIGCHLD, SIG_IGN);
+	signal(SIGINT, exitHandler);
 	pid = fork();
 	if (pid < 0)
 	{
@@ -119,13 +146,17 @@ int runCmds(Command *cmd,Pgm *p)
 		// printf("STDIN %s %s\n", cmd->rstdin, ps[0]);
 		dup2(pfds[0], STDIN_FILENO);
 		close(pfds[0]);
+		if (bgp != NULL && bgp == getpgid(getpid())) 
+		{
+			setpgid(getpid(), bgp);
+		}
 		waitpid(pid,NULL,NULL);
 		if ((execvp(ps[0],ps)) < 0)
 		{
 			return -1;
 		}
 	}
-	else // ch process
+	else // Child process
 	{
 		close(pfds[0]);
 		// printf("STDOUT %s %s\n", cmd->rstdout, ps[0]);
