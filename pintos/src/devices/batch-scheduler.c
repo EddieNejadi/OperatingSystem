bdabb0df13f6cabddc314362d9555060cd5228d0
Mehdi A.Nejadi <mehdi61b@gmail.com>
2014-12-08 20:49:52 +0100

    Small modification batchScheduler implementation

--- pintos/src/devices/batch-scheduler.c
+++ pintos/src/devices/batch-scheduler.c
@@ -140,8 +140,8 @@ void transferData(task_t task)
     /* FIXME implement */
     printf("Start transfering data into the buss...");
     int64_t ticks;
-    ticks = (int64_t) random_ulong();
-    timer_sleep(ticks);
+    //ticks = (int64_t) random_ulong();
+    //timer_sleep(ticks);
     printf("Leaving from the buss...");
     /*leaveSlot(task);*/
 }
