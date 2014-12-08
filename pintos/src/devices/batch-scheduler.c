afd019b1576b2e7de8bdc43e5c906ecd2d5d29b7
Mehdi A.Nejadi <mehdi61b@gmail.com>
2014-12-08 20:31:13 +0100

    Modified batchScheduler implementation

--- pintos/src/devices/batch-scheduler.c
+++ pintos/src/devices/batch-scheduler.c
@@ -48,6 +48,7 @@ void init_bus(void){
     
     /* msg("NOT IMPLEMENTED");*/
     /* FIXME implement */
+    bus_semaphore = (semaphore) {value = 0 }
     
     sema_init(bus_semaphore, BUS_CAPACITY);
 
