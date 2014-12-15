e98d2d2c675d1f42c4a23649fc359f3c59e35955
Mehdi A.Nejadi <mehdi61b@gmail.com>
2014-12-15 18:55:00 +0100

    Added batch-sheduler file

--- pintos/src/devices/batch-scheduler.c
+++ pintos/src/devices/batch-scheduler.c
@@ -23,6 +23,8 @@ typedef struct {
 	int priority;
 } task_t;
 
+static struct semaphore bus_semaphore;
+
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
         unsigned int num_priority_send, unsigned int num_priority_receive);
 
@@ -33,19 +35,22 @@ void receiverPriorityTask(void *);
 
 
 void oneTask(task_t task);/*Task requires to use the bus and executes methods below*/
-	void getSlot(task_t task); /* task tries to use slot on the bus */
-	void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
-	void leaveSlot(task_t task); /* task release the slot */
+void getSlot(task_t task); /* task tries to use slot on the bus */
+void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
+void leaveSlot(task_t task); /* task release the slot */
 
-
+    
 
 /* initializes semaphores */ 
 void init_bus(void){ 
  
     random_init((unsigned int)123456789); 
     
-    msg("NOT IMPLEMENTED");
+    /* msg("NOT IMPLEMENTED");*/
     /* FIXME implement */
+   // bus_semaphore = malloc(sizeof(bus_semaphore));  
+    
+    sema_init(&bus_semaphore, BUS_CAPACITY);
 
 }
 
@@ -63,32 +68,52 @@ void init_bus(void){
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
         unsigned int num_priority_send, unsigned int num_priority_receive)
 {
-    msg("NOT IMPLEMENTED");
+    /* msg("NOT IMPLEMENTED");*/
     /* FIXME implement */
+    char aux = 'n';
+    int i;
+    for(i = 0;i < num_tasks_send; i++)
+        {
+            thread_create("senderTask", PRI_DEFAULT, &senderTask, &aux);
+        }
+    for(i = 0;i < num_task_receive; i++)
+        {
+            thread_create("receiverTask", PRI_DEFAULT, &senderPriorityTask, &aux);
+        }
+    for(i = 0;i < num_priority_send; i++)
+        {
+            thread_create("senderPriorityTask", PRI_MAX, &receiverTask, &aux);
+        }
+    for(i = 0;i < num_priority_receive; i++)
+        {
+            thread_create("receiverPriorityTask", PRI_MAX, &receiverPriorityTask, &aux);
+        }
+
+
 }
 
 /* Normal task,  sending data to the accelerator */
 void senderTask(void *aux UNUSED){
-        task_t task = {SENDER, NORMAL};
-        oneTask(task);
+    task_t task = {SENDER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, sending data to the accelerator */
 void senderPriorityTask(void *aux UNUSED){
-        task_t task = {SENDER, HIGH};
-        oneTask(task);
+    task_t task = {SENDER, HIGH};
+    oneTask(task);
 }
 
 /* Normal task, reading data from the accelerator */
 void receiverTask(void *aux UNUSED){
-        task_t task = {RECEIVER, NORMAL};
-        oneTask(task);
+    task_t task = {RECEIVER, NORMAL};
+    oneTask(task);
 }
 
 /* High priority task, reading data from the accelerator */
 void receiverPriorityTask(void *aux UNUSED){
-        task_t task = {RECEIVER, HIGH};
-        oneTask(task);
+    task_t task = {RECEIVER, HIGH};
+    oneTask(task);
 }
 
 /* abstract task execution*/
@@ -102,20 +127,29 @@ void oneTask(task_t task) {
 /* task tries to get slot on the bus subsystem */
 void getSlot(task_t task) 
 {
-    msg("NOT IMPLEMENTED");
+    /* msg("NOT IMPLEMENTED"); */
     /* FIXME implement */
+    sema_down(&bus_semaphore);
+    /* transferData(task); */
 }
 
 /* task processes data on the bus send/receive */
 void transferData(task_t task) 
 {
-    msg("NOT IMPLEMENTED");
+    /* msg("NOT IMPLEMENTED"); */
     /* FIXME implement */
+    printf("Start transfering data into the buss...\n");
+    int64_t ticks;
+    ticks = (int64_t) random_ulong();
+    timer_sleep(ticks);
+    printf("Leaving from the buss...\n");
+    /*leaveSlot(task);*/
 }
 
 /* task releases the slot */
 void leaveSlot(task_t task) 
 {
-    msg("NOT IMPLEMENTED");
+    /* msg("NOT IMPLEMENTED"); */
     /* FIXME implement */
+    sema_up(&bus_semaphore);
 }
